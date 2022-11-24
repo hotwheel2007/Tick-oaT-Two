c6e277fc0f326dc7e5d0f2355da7708d32019053
hw2007 <60082961+hotwheel2007@users.noreply.github.com>
2022-11-23 18:44:01 -0600

    updated file to v2

--- Tick-oaT-Two.py
+++ Tick-oaT-Two.py
@@ -4,34 +4,47 @@
 
 from termcolor import colored
 
+# Creating vars
 board = [0, 0, 0,
         0, 0, 0,
         0, 0, 0]
+
 peices = [" ", colored("–", "green", attrs = ["bold"]), colored("|", "green", attrs = ["bold"]), colored("+", "green", attrs = ["bold", "dark"])]
 row_letters = ["a", "b", "c"]
 win_conditions = ["012", "345", "678", "036", "147", "258", "048", "246"]
+
 player = 1
 move = 0
+prev_move = 0
+
 peice_color = "green"
 peice_attrs = ["bold"]
 info_color = "yellow"
 
+
+# Printing credits
 print("")
 print(colored("==== Tick-oaT-Two ====", info_color))
 print(colored("Idea by: Oats Jenkins", info_color))
 print(colored("Programmed by: hw2007", info_color))
 print("")
 
+
+# Resets the board
 def reset():
     global board
     global player
+    global prev_move
 
     board = [0, 0, 0,
             0, 0, 0,
             0, 0, 0]
+
     player = 1
+    prev_move = 0
 
 
+# Draws the board
 def draw_board():
     print(colored("    1   2   3", "white", attrs = ["dark"]))
     print(colored("  –––––––––––––", "white", attrs = ["dark"]))
@@ -55,10 +68,11 @@ def draw_board():
     print(colored("  –––––––––––––", "white", attrs = ["dark"]))
 
 
-def place_piece(player, pos):
+# Places a piece
+def place_piece(player, pos, prev_pos):
     try:
         position = ((row_letters.index(pos[0]) * 3) + int(pos[1])) - 1
-        if board[position] == player or int(pos[1]) > 3:
+        if board[position] == player or int(pos[1]) > 3 or pos == prev_pos:
             print(colored("Invalid placement!", "red", attrs = ["bold"]))
             return(False)
         else:
@@ -74,6 +88,7 @@ def place_piece(player, pos):
         return(False)
 
 
+# Checks if a player has won the game
 def win():
     for condition in win_conditions:
         conditions_met = [0, 0, 0]
@@ -88,30 +103,38 @@ def win():
     return(False)
 
 
+# Game loop
 while True:
+    # Display the board
     print("")
     print("")
     print("")
     draw_board()
     print("")
 
+    # Get player input
     print("Player " + str(player) + ": Where would you like to place your piece?")
     move = input(colored("> ", "white", attrs = ["dark"]))
 
+    # Placing a piece
+    if place_piece(player, move, prev_move):
+        prev_move = move
 
-    if place_piece(player, move):
         if win():
+            # Display the board
             print("")
             print("")
             print("")
             draw_board()
             print("")
 
+            # Ask the player if they want to play again
             print(colored("PLAYER " + str(player) + " WINS!", "green", attrs = ["bold", "reverse"]))
             print("Would you like to play again? (y/n)")
             play_again = input(colored("> ", "white", attrs = ["dark"]))
             if play_again == "y": reset()
             else: exit()
         else:
+            # Change player
             if player == 1: player = 2
             else: player = 1
